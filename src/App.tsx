b89e409b625c0c4c48d7f568e71f4c44b12c5b6a
Jonny Lin <jonny@snippyly.com>
2023-02-13 23:14:44 -0800

    Added navbar icons

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,13 @@ import Chart from './components/Chart';
 import Table from './components/Table';
 import Metrics from './components/Metrics';
 
+import CalendarIcon from './icons/calendar.svg';
+import DownloadIcon from './icons/download.svg';
+import PieIcon from './icons/pie.svg';
+import ImgIcon from './icons/img.svg';
+import LightningIcon from './icons/lightning.svg';
+import SettingsIcon from './icons/settings.svg';
+
 const App = () => {
 
   const { client } = useSnippylyClient();
@@ -29,6 +36,17 @@ const App = () => {
     <Layout
       navbar={
         <>
+          <div className={styles['top-part']}>
+            <div className={styles['avatar']} />
+            <img src={CalendarIcon} />
+            <img src={DownloadIcon} />
+            <img src={LightningIcon} />
+            <img src={PieIcon} />
+            <img src={ImgIcon} />
+          </div>
+          <div className={styles['bottom-part']}>
+            <img src={SettingsIcon} />
+          </div>
         </>
       }
       content={
